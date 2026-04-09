07ad248d1231a5a9b5186cc1b8ecbb0d9f895f10
tatr42 <e69212317@gmail.com>
2026-04-09 23:52:17 +0300

    Firebase config guncellemesi Per 09.04.2026 23:52:17,42

--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -1,28 +1,29 @@
-export const dynamic = 'force-dynamic'; // Bu satırı en üste ekle
-
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-// Fonksiyonun tam olarak bu isimle (POST) dışa aktarıldığından emin ol
 export async function POST(req: Request) {
   try {
     const { password } = await req.json();
-    const ADMIN_PASSWORD = "Eb@Hac2027#Net";
 
-    if (password === ADMIN_PASSWORD) {
+    // Şifre kontrolü
+    if (password === "Eb@Hac2027#Net") {
       const response = NextResponse.json({ success: true });
+      
+      // Cookie ayarı - Versiyon uyumluluğu için await ekledik
       const cookieStore = await cookies();
       cookieStore.set("admin_session", "active", {
         httpOnly: true,
         secure: true,
         sameSite: "lax",
-        maxAge: 60 * 60 * 24,
+        maxAge: 60 * 60 * 24, // 1 gün
         path: "/",
       });
+
       return response;
     }
-    return NextResponse.json({ error: "Şifre yanlış" }, { status: 401 });
+
+    return NextResponse.json({ error: "Şifre Yanlış" }, { status: 401 });
   } catch (err) {
-    return NextResponse.json({ error: "Hata" }, { status: 500 });
+    return NextResponse.json({ error: "Sunucu Hatası" }, { status: 500 });
   }
 }
